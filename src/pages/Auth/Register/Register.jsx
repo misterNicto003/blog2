eb7fadd53f016c8a5deebdda7f070c115bf5d37a
Mlovz <mestoev.yakhya@mail.ru>
2023-03-14 22:23:55 +0300

    scss register

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-// import "./auth.scss";
+import { Heading, Input, Button } from "../../../components";
 import "../../Auth/auth.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="register_box">
+        <Heading>Регистрация</Heading>
+
+        <form className="register_box_form">
+          <Input name="Username" placeholder="Username" />
+          <Input name="Password" placeholder="Password" />
+          <Input name="Confirm Password" placeholder="Confirm Password" />
+          <Button>регистрация</Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
